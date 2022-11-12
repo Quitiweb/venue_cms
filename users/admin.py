01df8a8c3031809759c64ef17a93ce0a85622196
Quitiweb <rafa@quitiweb.com>
2022-11-12 17:48:19 +0100

    Add migrations and users admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import Account
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    """
+    Account model admin.
+    """
+    search_fields = ('username',)
+    ordering = ('username', 'is_superuser', 'is_active',)
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone', )}),
+    )
